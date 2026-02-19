1e508f851f768461b3c7a79537735982e9eabc8d
Tobias Ballesteros <tobiasrafael.ballesteros@gmail.com>
2026-02-19 16:09:12 -0300

    Analytics Nova

--- SQLGuardObservatory.API/obj/Release/net8.0/SQLGuardObservatory.API.AssemblyInfo.cs
+++ SQLGuardObservatory.API/obj/Release/net8.0/SQLGuardObservatory.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SQLGuardObservatory.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4ca29a66b61110d7223e00c101cb186d0159cf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1644accbbd47386ebf94e450b717cfde05da5b95")]
 [assembly: System.Reflection.AssemblyProductAttribute("SQLGuardObservatory.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SQLGuardObservatory.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
